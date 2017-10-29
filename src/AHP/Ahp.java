156f56d55ffb2903898e239a2259db17c3766f93
Zaldric <juancargm94@gmail.com>
2017-10-29 23:12:21 +0100

    Work finished.

--- src/AHP/Ahp.java
+++ src/AHP/Ahp.java
@@ -1,37 +1,88 @@
 package AHP;
+
 import org.apache.commons.math3.linear.EigenDecomposition;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 
 import java.util.ArrayList;
 
-public class Ahp {
+class Ahp {
 
-    public double[][] criteria;
-    public ArrayList<double[][]> alternatives;
-    private ArrayList<double[]> alternativeWeights;
+    double[][] criteria;
+    private ArrayList<Double> consistencyIndexes, consistencyRatios;
+    ArrayList<double[][]> alternatives;
 
-    public Ahp(int numberOfCriteria, int numberOfAlternatives) {
+    Ahp(int numberOfCriteria, int numberOfAlternatives) {
 
         criteria = new double[numberOfCriteria][];
         alternatives = new ArrayList<>();
+        consistencyIndexes = new ArrayList<>();
+        consistencyRatios = new ArrayList<>();
 
         for (int i = 0; i < numberOfCriteria; ++i) {
             double[][] matrix = new double[numberOfAlternatives][];
             alternatives.add(matrix);
         }
+    }
 
-        alternativeWeights = new ArrayList<>();
+    /**
+     *  Calculates the alternative's ranking using the approximated method.
+     *
+     * @return an array with the alternatives's ranking.
+     */
+    double[] approximatedMethod() {
+
+        double[] criteriaWeights = normalizeVector(calculateMatrixWeightsApproximated(criteria));
+        ArrayList<double[]> alternativeWeights = new ArrayList<>();
+
+        for (double[][] alternative : alternatives) {
+            alternativeWeights.add(normalizeVector(calculateMatrixWeightsApproximated(alternative)));
+        }
+
+        double[][] alternativesMatrix = new double[alternatives.size()][alternatives.get(0).length];
+
+        for (int i = 0; i < alternatives.size(); ++i) {
+            System.arraycopy(alternativeWeights.get(i), 0, alternativesMatrix[i],
+                    0, alternatives.get(0).length);
+        }
+
+        return aggregation(criteriaWeights, alternativesMatrix);
+    }
+
+    /**
+     *  Calculates the alternative's ranking using the geometric mean method.
+     *
+     * @return an array with the alternatives's ranking.
+     */
+    double[] geometricMeanMethod() {
+
+        double[] criteriaWeights = normalizeVector(calculateMatrixWeightsGeometricMean(criteria));
+        ArrayList<double[]> alternativeWeights = new ArrayList<>();
+
+        for (double[][] alternative : alternatives) {
+            alternativeWeights.add(normalizeVector(calculateMatrixWeightsGeometricMean(alternative)));
+        }
+
+        double[][] alternativesMatrix = new double[alternatives.size()][alternatives.get(0).length];
+
+        for (int i = 0; i < alternatives.size(); ++i) {
+            System.arraycopy(alternativeWeights.get(i), 0, alternativesMatrix[i],
+                    0, alternatives.get(0).length);
+        }
+
+        return aggregation(criteriaWeights, alternativesMatrix);
     }
 
     /**
      * Calculates the alternative's ranking using the eigenvalue method.
      *
-     * @return the array with the rankings.
+     * @return an array with the alternatives's ranking.
      */
-    public double[] eigenMethod() {
+    double[] eigenvalueMethod() {
 
         double[] criteriaWeights = normalizeVector(calculateMatrixWeightsEigenvalue(criteria));
+        ArrayList<double[]> alternativeWeights = new ArrayList<>();
 
         for (double[][] alternative : alternatives) {
             alternativeWeights.add(normalizeVector(calculateMatrixWeightsEigenvalue(alternative)));
@@ -40,10 +91,15 @@ public class Ahp {
         double[][] alternativesMatrix = new double[alternatives.size()][alternatives.get(0).length];
 
         for (int i = 0; i < alternatives.size(); ++i) {
-            for (int j = 0; j < alternatives.get(0).length; ++j) {
-                alternativesMatrix[i][j] = alternativeWeights.get(i)[j];
-            }
+            System.arraycopy(alternativeWeights.get(i), 0, alternativesMatrix[i],
+                    0, alternatives.get(0).length);
         }
+
+        calculateConsistencyRatios();
+        return aggregation(criteriaWeights, alternativesMatrix);
+    }
+
+    private double[] aggregation(double[] criteriaWeights, double [][] alternativesMatrix) {
 
         double[] ranking = new double[alternatives.get(0).length];
 
@@ -55,7 +111,47 @@ public class Ahp {
             ranking[i] = sum;
         }
 
-        return  ranking;
+        return ranking;
+    }
+
+    /**
+     * Calculates the matrix's weights using the approximated method.
+     *
+     * @return an array with the matrix's weight.
+     */
+    private double[] calculateMatrixWeightsApproximated(double[][] matrix) {
+
+        double[] resultVector = new double[matrix.length];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            double sum = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                sum += matrix[i][j];
+            }
+            resultVector[i] = sum;
+        }
+
+        return resultVector;
+    }
+
+    /**
+     * Calculates the matrix's weights using the geometric mean method.
+     *
+     * @return an array with the matrix's weight.
+     */
+    private double[] calculateMatrixWeightsGeometricMean(double[][] matrix) {
+
+        double[] resultVector = new double[matrix.length];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            double product = 1;
+            for (int j = 0; j < matrix.length; j++) {
+                product *= matrix[i][j];
+            }
+            resultVector[i] = Math.pow(product, 1.0/resultVector.length);
+        }
+
+        return resultVector;
     }
 
     /**
@@ -74,12 +170,14 @@ public class Ahp {
         double max = eigenValues[0];
         int pos = 0;
 
-        for (int i = 0; i < eigenValues.length; ++i) {
+        for (int i = 1; i < eigenValues.length; ++i) {
             if (eigenValues[i] > max) {
                 max = eigenValues[i];
                 pos = i;
             }
         }
+
+        consistencyIndexes.add((max - matrix.length) / (matrix.length - 1));
         return decomposition.getEigenvector(pos).toArray();
     }
 
@@ -104,4 +202,30 @@ public class Ahp {
 
         return normalizedVector;
     }
+
+    /**
+     * Calculates the consistency ratios for the AHP problem.
+     */
+    private void calculateConsistencyRatios() {
+
+        double[] randomIndex = new double[] {0.58, 0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49};
+
+        consistencyRatios.add(consistencyIndexes.get(0) / randomIndex[criteria.length - 3]);
+
+        for (int i = 0; i < criteria.length; ++i) {
+            consistencyRatios.add(consistencyIndexes.get(i + 1) / randomIndex[alternatives.get(0).length - 3]);
+        }
+    }
+
+    /**
+     *
+     * @return an ArrayList with the problem's consistency indexes.
+     */
+    ArrayList<Double> getConsistencyIndexes() { return consistencyIndexes;}
+
+    /**
+     *
+     * @return an ArrayList with the problem's consistency ratios.
+     */
+    ArrayList<Double> getConsistencyRatios() { return consistencyRatios;}
 }
--- src/AHP/Main.java
+++ src/AHP/Main.java
@@ -13,16 +13,186 @@ public class Main {
         ArrayList<double[][]> alternatives = new ArrayList<>();
 
         alternatives.add(new double[][]{{1,8,6}, {1/8d,1,1/3d}, {1/6d,3,1}});
-        alternatives.add(new double[][]{{1,5,1/3d}, {1/8d,1,1/3d}, {1/6d,3,1}});
+        alternatives.add(new double[][]{{1,5,1/3d}, {1/5d,1,1/7d}, {3,7,1}});
         alternatives.add(new double[][]{{1,1/7d,1/2d}, {7,1,4}, {2,1/4d,1}});
         alternatives.add(new double[][]{{1,1/8d,1/4d}, {8,1,3}, {4,1/3d,1}});
 
         ahp.alternatives = alternatives;
 
-        double[] ranking = ahp.eigenMethod();
+        System.out.println("Exercise 1: \n");
+        System.out.println("Approximated method: \n");
+
+        double[] ranking = ahp.approximatedMethod();
 
         for (int i = 0; i < ranking.length; ++i) {
-            System.out.println(ranking[i]);
+            System.out.println("Alternative " + (i + 1) + ": " + ranking[i]);
+        }
+
+        System.out.println();
+
+        double max = ranking[0];
+        int pos = 0;
+
+        for (int i = 1; i < ranking.length; ++i) {
+            if (ranking[i] > max) {
+                max = ranking[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+
+        System.out.println("Geometric mean method: \n");
+
+        double[] ranking2 = ahp.geometricMeanMethod();
+
+        for (int i = 0; i < ranking2.length; ++i) {
+            System.out.println("Alternative " + (i + 1) + ": " + ranking2[i]);
+        }
+
+        System.out.println();
+        max = ranking2[0];
+        pos = 0;
+
+        for (int i = 1; i < ranking2.length; ++i) {
+            if (ranking2[i] > max) {
+                max = ranking2[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+
+        System.out.println("Eigenvalue method: \n");
+        double[] ranking3 = ahp.eigenvalueMethod();
+
+        for (int i = 0; i < ranking3.length; ++i) {
+            System.out.println("Alternative " + (i + 1) + ": " + ranking3[i]);
+        }
+
+        System.out.println();
+        max = ranking3[0];
+        pos = 0;
+
+        for (int i = 1; i < ranking3.length; ++i) {
+            if (ranking3[i] > max) {
+                max = ranking3[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+
+        ArrayList<Double> consistencyRatios = ahp.getConsistencyRatios();
+        ArrayList<Double> consistencyIndexes = ahp.getConsistencyIndexes();
+
+        System.out.println("Consistency Indexes: \n");
+        System.out.println("Criteria: " + consistencyIndexes.get(0));
+
+        for (int i = 1; i < consistencyIndexes.size(); ++i) {
+            System.out.println("Alternative " + i + ": " + consistencyIndexes.get(i));
+        }
+
+        System.out.println();
+        System.out.println("Consistency Ratios: \n");
+        System.out.println("Criteria: " + consistencyRatios.get(0));
+
+        for (int i = 1; i < consistencyRatios.size(); ++i) {
+            System.out.println("Alternative " + i + ": " + consistencyRatios.get(i));
+        }
+
+        Ahp ahp2 = new Ahp(4, 3);
+
+        ahp2.criteria = new double[][]{{1,2,6,2}, {1/2d,1,7,1}, {1/6d,1/7d,1,5}, {1/2d,1,1/5d,1}};
+
+        ArrayList<double[][]> alternatives2 = new ArrayList<>();
+
+        alternatives2.add(new double[][]{{1,1/6d,6}, {6,1,4}, {1/6d,1/4d,1}});
+        alternatives2.add(new double[][]{{1,5,2}, {1/5d,1,1/7d}, {1/2d,7,1}});
+        alternatives2.add(new double[][]{{1,1/4d,1/2d}, {4,1,4}, {2,1/4d,1}});
+        alternatives2.add(new double[][]{{1,1/5d,1/2d}, {5,1,1}, {2,1,1}});
+
+        ahp2.alternatives = alternatives2;
+
+        System.out.println();
+        System.out.println("Exercise 2: \n");
+        System.out.println("Approximated method: \n");
+
+        double[] ranking4 = ahp2.approximatedMethod();
+
+        for (int i = 0; i < ranking4.length; ++i) {
+            System.out.println("Alternative " + (i + 1) + ": " + ranking4[i]);
+        }
+
+        System.out.println();
+        max = ranking4[0];
+        pos = 0;
+
+        for (int i = 1; i < ranking4.length; ++i) {
+            if (ranking4[i] > max) {
+                max = ranking4[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+
+        System.out.println("Geometric mean method: \n");
+
+        double[] ranking5 = ahp2.geometricMeanMethod();
+
+        for (int i = 0; i < ranking5.length; ++i) {
+            System.out.println("Alternative " + (i + 1) + ": " + ranking5[i]);
+        }
+
+        System.out.println();
+        max = ranking5[0];
+        pos = 0;
+
+        for (int i = 1; i < ranking5.length; ++i) {
+            if (ranking5[i] > max) {
+                max = ranking5[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+        System.out.println("Eigenvalue method: \n");
+        double[] ranking6 = ahp2.eigenvalueMethod();
+
+        for (int i = 0; i < ranking6.length; ++i) {
+            System.out.println("Alternative " + (i + 1) + ": " + ranking6[i]);
+        }
+
+        System.out.println();
+        max = ranking6[0];
+        pos = 0;
+
+        for (int i = 1; i < ranking6.length; ++i) {
+            if (ranking6[i] > max) {
+                max = ranking6[i];
+                pos = 1;
+            }
+        }
+
+        System.out.println("The best alternative is: Alternative " + (pos + 1) + "\n");
+
+        ArrayList<Double> consistencyRatios2 = ahp2.getConsistencyRatios();
+        ArrayList<Double> consistencyIndexes2 = ahp2.getConsistencyIndexes();
+
+        System.out.println("Consistency Indexes: \n");
+        System.out.println("Criteria: " + consistencyIndexes2.get(0));
+
+        for (int i = 1; i < consistencyIndexes2.size(); ++i) {
+            System.out.println("Alternative " + i + ": " + consistencyIndexes2.get(i));
+        }
+
+        System.out.println();
+        System.out.println("Consistency Ratios: \n");
+        System.out.println("Criteria: " + consistencyRatios2.get(0));
+
+        for (int i = 1; i < consistencyRatios2.size(); ++i) {
+            System.out.println("Alternative " + i + ": " + consistencyRatios2.get(i));
         }
     }
 }
